fb8d9cbe3007fb76ba01ea980229e5b0be5bf9df
Nikita V <qask_platform@mail.ru>
2020-05-22 16:32:18 +0300

    [ v1.1 ] Added omitempty param to json field "Comment"

--- internal/app/model/question.go
+++ internal/app/model/question.go
@@ -5,5 +5,5 @@ type Question struct {
 	Sitename string `json:"sitename"`
 	Question string `json:"question"`
 	Answer   string `json:"answer"`
-	Comment  string `json:"comment"`
+	Comment  string `json:"comment,omitempty"`
 }
