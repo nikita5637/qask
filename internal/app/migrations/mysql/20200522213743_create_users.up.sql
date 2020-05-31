2073835440948283ab7afdce1558a182bb58a24f
Nikita V <qask_platform@mail.ru>
2020-05-31 10:03:16 +0300

    [ v1.1 ] Changed MySQL up migration

--- internal/app/migrations/mysql/20200522213743_create_users.up.sql
+++ internal/app/migrations/mysql/20200522213743_create_users.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id bigint not null primary key,
+    id bigint AUTO_INCREMENT UNIQUE not null primary key,
     tgid bigint null,
     firstname varchar(100) not null,
     username varchar(100) not null
