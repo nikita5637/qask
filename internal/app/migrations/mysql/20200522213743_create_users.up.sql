4c369e54a010fe1c981a0d055dc9882ecd08701c
Nikita V <qask_platform@mail.ru>
2020-05-31 10:28:36 +0300

    [ v1.1 ] Changed MySQL up migration

--- internal/app/migrations/mysql/20200522213743_create_users.up.sql
+++ internal/app/migrations/mysql/20200522213743_create_users.up.sql
@@ -1,6 +1,12 @@
-CREATE TABLE users (
-    id bigint AUTO_INCREMENT UNIQUE not null primary key,
-    tgid bigint null,
-    firstname varchar(100) not null,
-    username varchar(100) not null
-);
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` bigint(20) NOT NULL,
+    `tgid` bigint(20) DEFAULT NULL,
+    `username` varchar(100) NOT NULL,
+    `firstname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `users`
+    ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `users`
+    MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
